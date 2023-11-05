ef09f070d82fb7aa1554e24b0717c022128bc84b
jaeilssss <jaeilssss@bitbucket.org>
2023-11-05 16:49:09 +0900

    first commit

---
+++ weightedMovingAverage/WeightedMovingAverage.cpp
@@ -0,0 +1,8 @@
+//
+//  WeightedMovingAverage.cpp
+//  weightedMovingAverage
+//
+//  Created by Jaeil Jeong on 2023/11/04.
+//
+
+#include "WeightedMovingAverage.hpp"
---
+++ weightedMovingAverage/WeightedMovingAverage.hpp
@@ -0,0 +1,13 @@
+//
+//  WeightedMovingAverage.hpp
+//  weightedMovingAverage
+//
+//  Created by Jaeil Jeong on 2023/11/04.
+//
+
+#ifndef WeightedMovingAverage_hpp
+#define WeightedMovingAverage_hpp
+
+#include <stdio.h>
+
+#endif /* WeightedMovingAverage_hpp */
